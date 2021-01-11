51404b31a0db94ab58e72aec3f148f313eb04ae5
Nathan Peek <nmp14fsu@gmail.com>
2021-01-11 05:07:05 -0500

    Create global variables for html elements

---
+++ script.js
@@ -0,0 +1,4 @@
+let highScores = document.getElementById("high-scores");
+let timeCounter = document.getElementById("time-count");
+let container = document.getElementById("quiz-container");
+let startBtn = document.getElementById("start-button")
