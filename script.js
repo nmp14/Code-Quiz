31562f4e035b69529fd5f9f41cc8b36ee7448ca6
Nathan Peek <nmp14fsu@gmail.com>
2021-01-12 01:48:25 -0500

    Add click event for start button and initial content for start function

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
 let highScores = document.getElementById("high-scores");
 let timeCounter = document.getElementById("time-count");
 let container = document.getElementById("quiz-container");
-let startBtn = document.getElementById("start-button")
+let startBtn = document.getElementById("start-button");
+let newBtn = document.createElement("button");
+
+function questionOne() {
+
+}
+
+function start() {
+    // Hide the start button
+    startBtn.style.display = "none";
+    // Generate a button for submitting questions.
+    addButton();
+    // Start timer
+    //...
+    // Call question one.
+    questionOne();
+}
+
+function addButton() {
+    newBtn.innerHTML = "Submit";
+    newBtn.setAttribute("class", "btn my-5 text-light");
+    container.appendChild(newBtn);
+}
+
+startBtn.addEventListener("click", start);
